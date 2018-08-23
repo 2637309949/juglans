167142d2fb44983ff2b86c8183388ffa326fb1ee
2637309949@qq.com <2637309949@qq.com>
2018-08-24 00:10:11 +0800

    deta12

--- example/hello/src/config/local.js
+++ example/hello/src/config/local.js
@@ -10,8 +10,20 @@ module.exports = {
       reconnectTries: Number.MAX_VALUE
     }
   },
+  debug: true,
   redis: 'redis://:F7B73743E7AEDD58E58900F4782550BF@www.wosoft.me:6412',
-  injectPath: [path.join(__dirname, '../models/**/*.js'), path.join(__dirname, '../routes/**/*.js')],
   jwtSecret: 'ef6d85d2a46311e8aa557555c34ad35a',
-  ignorePath: ['**/node_modules/**']
+  ignorePath: ['**/node_modules/**'],
+  injectPath: [path.join(__dirname, '../models/**/*.js'), path.join(__dirname, '../routes/**/*.js')],
+  koaBody: {
+    strict: false,
+    jsonLimit: '5mb',
+    formLimit: '1mb',
+    textLimit: '1mb',
+    multipart: true,
+    formidable: {
+      keepExtensions: true,
+      uploadDir: path.join(__dirname, '../assets/public/upload')
+    }
+  }
 }
