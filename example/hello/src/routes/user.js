5c2d034b5657c55b1aef27ad189666b60d241752
2637309949@qq.com <2637309949@qq.com>
2018-08-25 23:34:55 +0800

    beta14

--- example/hello/src/routes/user.js
+++ example/hello/src/routes/user.js
@@ -7,12 +7,15 @@ module.exports = function ({ mongoose, router, test }) {
   router.get('/test', (ctx, next) => {
     ctx.body = 'test:' + test
   })
+
   router.get('/hello', (ctx, next) => {
     ctx.body = 'hello:' + test
   })
+
   router.get('/user/aux/manager', async (ctx) => {
     try {
-      const username = ctx.query.username || ctx.request.body.username
+      let username = ctx.query.username
+      username = username || ctx.request.body.username
       const isManager = await userServices.isManager({mongoose}, username)
       ctx.body = {data: isManager}
     } catch (error) {
