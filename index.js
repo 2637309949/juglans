b0d991b7001762015014a97c1bc2aa71c506755d
2637309949@qq.com <2637309949@qq.com>
2018-09-02 18:48:24 +0800

    beta20

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const core = require('./lib/core')
+/**
+ * @author [Double]
+ * @email [2637309949@mail.com]
+ * @create date 2018-09-02 12:51:45
+ * @modify date 2018-09-02 12:51:45
+ * @desc [导出根对象]
+*/
+const core = require('./lib/juglans')
 module.exports = core
