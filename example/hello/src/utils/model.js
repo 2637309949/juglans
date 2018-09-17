ab47dfa8adf5ce995025239e32b6ab60e960386b
2637309949@qq.com <2637309949@qq.com>
2018-09-17 22:19:25 +0800

    beta42

--- example/hello/src/utils/model.js
+++ example/hello/src/utils/model.js
@@ -1,26 +1,25 @@
+
+const Juglans = require('../../../..')
+const mongoose = Juglans.mongoose
+
 /**
  * 注册model
  * @param {Object} config
  * @param {Object} mongoose
  */
-const tokenMoel = (config, mongoose) => {
+module.exports = () => {
   const AccessToken = mongoose.model('AccessToken')
   return {
-    save: async function (item) {
-      return AccessToken.create([item])
-    },
-    find: async function (accessToken) {
-      return AccessToken.findOne({ accessToken, _dr: false })
-    },
-    delete: async function (accessToken) {
-      return AccessToken.updateOne({ accessToken, _dr: false }, { $set: { _dr: true } })
+    token: {
+      async save (item) {
+        return AccessToken.create([item])
+      },
+      async find (accessToken) {
+        return AccessToken.findOne({ accessToken, _dr: false })
+      },
+      async delete (accessToken) {
+        return AccessToken.updateOne({ accessToken, _dr: false }, { $set: { _dr: true } })
+      }
     }
-  }
-}
-
-module.exports = ({config, mongoose}) => {
-  const token = tokenMoel(config, mongoose)
-  return {
-    token
   }
 }
