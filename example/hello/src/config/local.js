8b1b73b1b5f4b79fde71d4f7166f2099302c1f21
2637309949@qq.com <2637309949@qq.com>
2018-10-02 16:49:38 +0800

    优化example

--- example/hello/src/config/local.js
+++ example/hello/src/config/local.js
@@ -24,7 +24,7 @@ module.exports = {
     login: '/login',
     logout: '/logout',
     fakeTokens: ['DEBUG'],
-    fakeUrls: [/\/test$/]
+    fakeUrls: [/\/test$/, /\/favicon\.ico$/]
   },
   koaBodyOpts: {
     strict: false,
--- example/hello/src/utils/model.js
+++ example/hello/src/utils/model.js
@@ -7,21 +7,20 @@ const mongoose = Juglans.mongoose
  * @param {Object} config
  * @param {Object} mongoose
  */
-module.exports = () => {
-  const AccessToken = mongoose.model('AccessToken')
-  return {
-    token: {
-      async save (item) {
-        return AccessToken.create([item])
-      },
-      async find (accessToken) {
-        let result = await AccessToken.findOne({ accessToken, _dr: false })
-        result = (result && result.toJSON && result.toJSON()) || result
-        return result
-      },
-      async delete (accessToken) {
-        return AccessToken.updateOne({ accessToken, _dr: false }, { $set: { _dr: true } })
-      }
+module.exports = {
+  TOKEN: {
+    async save (item) {
+      const AccessToken = mongoose.model('AccessToken')
+      return AccessToken.create([item])
+    },
+    async find (accessToken) {
+      const AccessToken = mongoose.model('AccessToken')
+      const result = await AccessToken.findOne({ accessToken, _dr: false })
+      return result && result._doc
+    },
+    async delete (accessToken) {
+      const AccessToken = mongoose.model('AccessToken')
+      return AccessToken.updateOne({ accessToken, _dr: false }, { $set: { _dr: true } })
     }
   }
 }
