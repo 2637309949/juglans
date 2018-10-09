a8a383562cc161ef59a53636b66efb6f4ac67075
2637309949@qq.com <2637309949@qq.com>
2018-10-09 14:41:24 +0800

    update .gitignore

--- example/hello/src/assets/public/apidoc/api_project.js
+++ example/hello/src/assets/public/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-10-09T06:39:58.495Z",
+    "time": "2018-10-09T06:40:39.863Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
