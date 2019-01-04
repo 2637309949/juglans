787e84e622a297ffaedb22abfbfc0f337775cdab
2637309949@qq.com <2637309949@qq.com>
2019-01-05 03:22:25 +0800

    框架优化

--- index.js
+++ index.js
@@ -3,12 +3,9 @@
  * @email [2637309949@mail.com]
  * @create date 2018-09-02 12:51:45
  * @modify date 2018-09-02 12:51:45
- * @desc [导出根对象]
+ * @desc [export root object]
 */
 module.exports = require('./lib/Juglans')
-
-// 其他对象导出
-module.exports.mongoose = require('./lib/mongoose')
-module.exports.Redis = require('./lib/redis')
 module.exports.Plugins = require('./lib/plugins')
 module.exports.utils = require('./lib/utils')
+module.exports = Object.assign(module.exports, require('./lib/addition'))
