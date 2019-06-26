60423b51e0e3ff8ad86c1053bccde2c72d1428fc
double <2637309949@qq.com>
2019-06-26 15:00:17 +0800

    feat: logger

--- dist/logger.js
+++ dist/logger.js
@@ -3,26 +3,6 @@
 // Copyright (c) 2018-2020 Double.  All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
-const winston = require('winston');
+const logger = require('juglans-addition').logger;
 
-const {
-  combine,
-  timestamp,
-  printf,
-  colorize
-} = winston.format;
-const format = combine(colorize(), timestamp(), printf((_ref) => {
-  let {
-    level,
-    message,
-    timestamp
-  } = _ref;
-  return `[${level}]: ${timestamp} ${message}`;
-}));
-module.exports = winston.createLogger({
-  level: 'info',
-  format,
-  transports: [new winston.transports.Console({
-    format
-  })]
-});
+module.exports = logger;
