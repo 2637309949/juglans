f4cef7e2d2cfb0f5472d380b082b2bdf1a06676b
double <2637309949@qq.com>
2019-08-27 15:19:26 +0800

    styles: 代码风格调整

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,5 @@
 "use strict";
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
-
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } }
-
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
@@ -310,8 +302,7 @@ function () {
     try {
       const _this = this;
 
-      this.Use.apply(this, _toConsumableArray(_this.scanPlugins));
-      yield runPlugins([].concat(_toConsumableArray(_this.prePlugins), _toConsumableArray(_this.plugins), _toConsumableArray(_this.postPlugins)), () => _this.injects, {
+      yield runPlugins(this.prePlugins.Append(_this.plugins).Append(_this.scanPlugins).Append(_this.postPlugins), () => _this.injects, {
         execAfter(ret) {
           _this.Inject(ret);
         }
